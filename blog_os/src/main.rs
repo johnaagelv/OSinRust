632a09f6d80e70f4dedcf47a1ae8417f734e7392
johnaagelv <j.andersen.lv@gmail.com>
2025-05-16 12:58:12 +0300

    Starting the OS kernel

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -8,9 +8,18 @@
 // Include the PanicInfo for our own panic handler
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello world";
+
 // Define the entry point as _start using C calling convention
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+unsafe extern "C" fn _start() -> ! {
+	let vga_bugger = 0xb8000 as *mut u8;
+
+	for (i, &byte) in HELLO.iter().enumerate() {
+		unsafe {
+			*vga_buffer.offset(i as isize * 2) = byte;
+			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+		}
+	}
 	// Loop forever
 	loop {}
 }
